de2ba74a8430bca936a0daf733e7a64bbcae7abd
Oleksii Prudkyi <Oleksii.Prudkyi@gmail.com>
2016-08-15 16:58:31 +0300

    initial commit

---
+++ _error_handler.js
@@ -0,0 +1,38 @@
+/*
+ * This file is part of js error alerter - tool to simplify catching of javascript errors
+ *
+ * Main error handler, helps to popup errors earlier
+ * place it atop of page as possible, or even embed it into page
+ *
+ * MIT licensed
+ * (c) 2016 Oleksii Prudkyi <Oleksii.Prudkyi@gmail.com>
+ */
+
+
+/* you should have this function defined atop of your page or first js file
+var showUncaughtException = function(message) {
+	"use strict";
+
+	if(typeof message === "undefined") {
+		return false;
+	}
+	alert(message);
+};
+*/
+
+window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+	"use strict";
+
+	if(typeof errorMsg == "undefined") {
+		return false;
+	}
+
+	showUncaughtException("JS Error : " + errorMsg + "\n" +
+		"line number : " + lineNumber + "\n" +
+		"column : " + column + "\n" +
+		"object : " + errorObj
+		);
+        
+    // Tell browser to run its own error handler as well   
+    return false;
+};
